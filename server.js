74680e96638dd59b8e8d6a9e495e69aeac4c2472
SwanHtetMyintAung <swan60953@gmail.com>
2025-07-12 17:59:17 +0630

    port binds

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-
+require("dotenv").config();
 app.all("/",(req,res)=>{
     res.send("All is Well")
 })
 
 function keepAlive(){
-    app.listen(3000,(req,res)=>{
+    app.listen(process.env.PORT || 3000,(req,res)=>{
         console.log("Shit is Running.")
     })
 }
